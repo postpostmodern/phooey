c8a78230b33277cec9587b75ae249c483c37a0aa
Jason T Johnson <design@postpostmodern.com>
2009-03-04 13:06:17 -0600

    Removed some unneeded js files and updated some template stuff

--- public/js/rollovers.js
+++ public/js/rollovers.js
@@ -1,27 +1,29 @@
+/* Requires jQuery */
+
 $(document).ready(function() {
-	
-	// Preload all rollovers
-	$("a.rollover > img").each(function() {
-		// Set the original src
-		rollsrc = $(this).attr("src");
-		rollON = rollsrc.replace(/.gif$/ig,"_OVER.gif").replace(/.jpg$/ig,"_OVER.jpg");
-		$("<img>").attr("src", rollON);
-	});
-	
-	// Link rollovers
-	$("a.rollover").mouseover(function(){
-		imgsrc = $(this).children("img").attr("src");
-		matches = imgsrc.match(/_OVER/);
-		
-		// don't do the rollover if state is already ON
-		if (!matches) {
-		  imgsrcON = imgsrc.replace(/.gif$/ig,"_OVER.gif").replace(/.jpg$/ig,"_OVER.jpg"); // strip off extension
-		  $(this).children("img").attr("src", imgsrcON);
-		}
-		
-	});
-	$("a.rollover").mouseout(function(){
-		$(this).children("img").attr("src", imgsrc);
-	});
-	
+  
+  // Preload all rollovers
+  $("a.rollover img, input.rollover, img.rollover").each(function() {
+    // Set the original src
+    rollsrc = $(this).attr("src");
+    rollON = rollsrc.replace(/.gif$/ig,"_OVER.gif").replace(/.jpg$/ig,"_OVER.jpg");
+    $("<img>").attr("src", rollON);
+  });
+  
+  // Link rollovers
+  $("a.rollover img, input.rollover, img.rollover").mouseover(function(){
+    imgsrc = $(this).attr("src");
+    matches = imgsrc.match(/_OVER/);
+    
+    // don't do the rollover if state is already ON
+    if (!matches) {
+      imgsrcON = imgsrc.replace(/.gif$/ig,"_OVER.gif").replace(/.jpg$/ig,"_OVER.jpg"); // strip off extension
+      $(this).attr("src", imgsrcON);
+    }
+    
+  });
+  $("a.rollover img, input.rollover, img.rollover").mouseout(function(){
+    $(this).attr("src", imgsrc);
+  });
+  
 });
